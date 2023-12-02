2ed71fddd7a06a0913c475c2fc7514172fd1dc51
Ryan Kupka <ryankupk@gmail.com>
2023-12-01 21:09:53 -0700

    add base file

---
+++ base.py
@@ -0,0 +1,16 @@
+def parse_input(filename: str):
+    pass
+
+def part_one(strings: list):
+    pass
+ 
+def part_two(strings: list):
+    pass
+
+def main(input_filename: str):
+    inp = parse_input(input_filename)
+    part_one(inp)
+    part_two(inp)
+
+if __name__ == "__main__":
+    main("sample.txt")
