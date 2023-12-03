9fab53a690adb629b75da8d1110c8887f7069da6
Ryan Kupka <ryankupk@gmail.com>
2023-12-03 12:10:51 -0700

    complete day3

--- day3/day3.py
+++ day3/day3.py
@@ -121,6 +121,124 @@ def find_adjacent_nums(symbol_x: int, symbol_y: int, strings: list):
     
     return sum(current_nums)
 
+def find_adjacent_nums_gears(symbol_x: int, symbol_y: int, strings: list):
+    current_nums = []
+    skip_up = False
+    skip_up_right = False
+    skip_down = False
+    skip_down_right = False
+    
+    if symbol_x - 1 >= 0 and symbol_y - 1 >= 0 and\
+        strings[symbol_y - 1][symbol_x - 1] in INTS:
+            
+        current_num = ''
+        temp_x = symbol_x - 1
+        
+        while temp_x >= 0 and strings[symbol_y - 1][temp_x] in INTS:
+            current_num += strings[symbol_y - 1][temp_x]
+            temp_x -= 1
+        temp_x = symbol_x
+        current_num = current_num[::-1]
+        
+        while temp_x < ROW_LEN and strings[symbol_y - 1][temp_x] in INTS:
+            if skip_up:
+                skip_up_right = True
+            current_num += strings[symbol_y - 1][temp_x]
+            temp_x += 1
+            skip_up = True
+
+        current_nums.append(int(current_num))
+            
+    if symbol_y - 1 >= 0 and\
+        not skip_up and\
+        strings[symbol_y - 1][symbol_x] in INTS:
+        
+        current_num = ''
+        temp_x = symbol_x
+        while temp_x < ROW_LEN and strings[symbol_y - 1][temp_x] in INTS:
+            current_num += strings[symbol_y - 1][temp_x]
+            temp_x += 1
+            skip_up_right = True
+
+        current_nums.append(int(current_num))
+        
+    if symbol_x + 1 < ROW_LEN and symbol_y - 1 >= 0 and\
+        not skip_up_right and\
+        strings[symbol_y - 1][symbol_x + 1] in INTS:
+
+        current_num = ''
+        temp_x = symbol_x + 1
+        while temp_x < ROW_LEN and strings[symbol_y - 1][temp_x] in INTS:
+            current_num += strings[symbol_y - 1][temp_x]
+            temp_x += 1
+        current_nums.append(int(current_num))
+    
+    if symbol_x - 1 >= 0 and\
+        strings[symbol_y][symbol_x - 1] in INTS:
+        current_num = ''
+        temp_x = symbol_x - 1
+        while temp_x >= 0 and strings[symbol_y][temp_x] in INTS:
+            current_num += strings[symbol_y][temp_x]
+            temp_x -= 1
+        current_nums.append(int(current_num[::-1]))
+        
+    if symbol_x + 1 < ROW_LEN and\
+        strings[symbol_y][symbol_x + 1] in INTS:
+        current_num = ''
+        temp_x = symbol_x + 1
+        while temp_x < ROW_LEN and strings[symbol_y][temp_x] in INTS:
+            current_num += strings[symbol_y][temp_x]
+            temp_x += 1
+        current_nums.append(int(current_num))
+    
+    if symbol_x - 1 >= 0 and symbol_y + 1 < COL_LEN and\
+        strings[symbol_y + 1][symbol_x - 1] in INTS:
+        current_num = ''
+        temp_x = symbol_x - 1
+        while temp_x >= 0 and strings[symbol_y + 1][temp_x] in INTS:
+            current_num += strings[symbol_y + 1][temp_x]
+            temp_x -= 1
+        temp_x = symbol_x
+        current_num = current_num[::-1]
+        
+        while temp_x < ROW_LEN and strings[symbol_y + 1][temp_x] in INTS:
+            if skip_down:
+                skip_down_right = True
+            current_num += strings[symbol_y + 1][temp_x]
+            temp_x += 1
+            skip_down = True
+
+        current_nums.append(int(current_num))
+    
+    if symbol_y + 1 < COL_LEN and\
+        not skip_down and\
+        strings[symbol_y + 1][symbol_x] in INTS:
+        
+        current_num = ''
+        temp_x = symbol_x
+        while temp_x < ROW_LEN and strings[symbol_y + 1][temp_x] in INTS:
+            current_num += strings[symbol_y + 1][temp_x]
+            temp_x += 1
+            skip_down_right = True
+
+        current_nums.append(int(current_num))
+    
+    if symbol_x + 1 < ROW_LEN and symbol_y + 1 < COL_LEN and\
+        not skip_down_right and\
+        strings[symbol_y + 1][symbol_x + 1] in INTS:
+
+        current_num = ''
+        temp_x = symbol_x + 1
+        while temp_x < ROW_LEN and strings[symbol_y + 1][temp_x] in INTS:
+            current_num += strings[symbol_y + 1][temp_x]
+            temp_x += 1
+        current_nums.append(int(current_num))
+    
+    if len(current_nums) == 2:
+        return current_nums[0] * current_nums[1]
+    else: return 0
+
+
 def part_one(strings: list):
     global COL_LEN
     COL_LEN = len(strings)
@@ -136,10 +254,16 @@ def part_one(strings: list):
             
     print(parts_total)
                 
-                
- 
 def part_two(strings: list):
-    pass
+    
+    ratio_total = 0
+    for y, line in enumerate(strings):
+        for x, char in enumerate(line):
+            if char != '*': continue
+
+            ratio_total += find_adjacent_nums_gears(x, y, strings)
+            
+    print(ratio_total)
 
 def main(input_filename: str):
     inp = parse_input(input_filename)
