b17c67cda3bf80491b841339b0ede804bded7905
Ryan Kupka <ryankupk@gmail.com>
2023-12-16 18:21:53 -0700

    refactor day5

--- day5/day5.py
+++ day5/day5.py
@@ -1,32 +1,29 @@
 import re
-from multiprocessing import Pool
+import multiprocessing
 
-def parse_input(filename: str) -> list[list[list[int]]]:
+maps: list[list[list[int]]] = []
+
+def parse_input(filename: str) -> list[int]:
     seeds = None
     groups = None
-    maps = []
     with open(filename, "r") as f:
         seeds = list(map(int, re.search(r"seeds: (.*)", f.readline()).groups()[0].split(' ')))
         f.readline()
         groups = [group.split('\n') for group in f.read().split('\n\n')]
-    for group in groups:
-        cur_map = []
-        for line in group[1:]: # skip name of map
-            cur_map.append(list(map(int, line.strip().split(' '))))
+    global maps
+    maps = [[list(map(int, line.strip().split(' '))) for line in group[1:]] for group in groups]
             
-        maps.append(cur_map)
-    return seeds, maps
+    return seeds
 
 
-def bounds_check(map_data, item):
+def bounds_check(map_data: list[list[int]], item: int) -> int:
     for dest, source, length in map_data:
         if source <= item < source + length:
             return dest + (item - source)
     else:
         return item
 
-def get_location(seed: int, maps: list[list[list[int]]]):
-    
+def get_location(seed: int) -> int:
     soil = bounds_check(maps[0], seed)
     fert = bounds_check(maps[1], soil)
     water = bounds_check(maps[2], fert)
@@ -36,30 +33,26 @@ def get_location(seed: int, maps: list[list[list[int]]]):
     location = bounds_check(maps[6], humid)
     return location
 
-def part_one(seeds: list[int], maps: list[list[list[int]]]):
-    locations = []
-    
-    for seed in seeds:
-        locations.append(get_location(seed, maps))
-    
-    print(min(locations))
- 
-def part_two(seeds, maps):
+def get_min_location_for_seed_range(seeds: tuple[int, int]) -> int:
     min_location = float('inf')
-    for idx, seed in enumerate(seeds):
-        if idx % 2 == 1: continue
-        for seed in range(seed, seed + seeds[idx+1]):
-            cur_location = get_location(seed, maps)
-            min_location = min(min_location, cur_location)
-        print(f"completed iteration {idx}")
-        
+    for seed in range(seeds[0], seeds[0] + seeds[1]):
+        min_location = min(min_location, get_location(seed))
+    return min_location
+
+def part_one(seeds: list[int]) -> int:
+    print(min([get_location(seed) for seed in seeds]))
+ 
+def part_two(seeds: list[int]) -> int:
+    seed_sets = [(seeds[i], seeds[i + 1]) for i in range(0, len(seeds), 2)]
+    with multiprocessing.Pool() as pool:
+        min_locations = pool.map(get_min_location_for_seed_range, seed_sets)
     
-    print(min_location)
+    print(min(min_locations))
 
 def main(input_filename: str):
-    seeds, maps = parse_input(input_filename)
-    part_one(seeds, maps)
-    part_two(seeds, maps)
+    seeds = parse_input(input_filename)
+    part_one(seeds)
+    part_two(seeds)
 
 if __name__ == "__main__":
-    main("sample.txt")
+    main("input.txt")
