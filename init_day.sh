006c32b45528a606f4cbd5ded5219bb68697acdb
Ryan Kupka <ryankupk@gmail.com>
2024-01-06 17:56:30 -0700

    refactor init_day script to utilize the year

--- init_day.sh
+++ init_day.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
-while getopts d: flag
+while getopts d:y: flag
 do
     case "$flag" in 
         d) day_num=${OPTARG};;
+        y) year=${OPTARG};;
     esac
 done
 
-mkdir -p "day$day_num"
-cp ./base.py "day$day_num/day$day_num.py"
-curl "https://adventofcode.com/2023/day/$day_num/input" -H "Cookie: session=$AOC_SESSION_ID" > "day$day_num/input.txt"
-head -4 "day$day_num/input.txt" > "day$day_num/sample.txt"
+mkdir -p "./$year/day$day_num"
+cp ./base.py "./$year/day$day_num/day$day_num.py"
+curl "https://adventofcode.com/$year/day/$day_num/input" -H "Cookie: session=$AOC_SESSION_ID" > "./$year/day$day_num/input.txt"
+head -4 "./$year/day$day_num/input.txt" > "./$year/day$day_num/sample.txt"
