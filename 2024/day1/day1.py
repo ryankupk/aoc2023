ee2c5f9c1f2691d197e44d8aa944e3bda2326820
ryankupk <ryankupk@gmail.com>
2024-11-30 23:34:50 -0700

    Use heap in part one, use defaultdict in part two

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -1,4 +1,5 @@
-from collections import Counter
+from collections import defaultdict
+import heapq
 import time
 
 def parse_input(filename: str) -> list[str]:
@@ -7,26 +8,24 @@ def parse_input(filename: str) -> list[str]:
         return rows
 
 def part_one(strings: list[str]) -> None:
-    left = []
-    right = []
+    left, right = [], []
     for row in strings:
-        left.append(int(row[0]))
-        right.append(int(row[1]))
-    left.sort()
-    right.sort()
-    res = []
-    for a, b in zip(left, right):
-        res.append(abs(a-b))
+        heapq.heappush(left, int(row[0]))
+        heapq.heappush(right, int(row[1]))
+    res = 0
+    while len(left) > 0:
+        a, b = heapq.heappop(left), heapq.heappop(right)
+        res += (abs(a-b))
 
-    print(sum(res))
+    print(res)
  
 def part_two(strings: list[str]) -> None:
     left = []
-    right = []
+    right_counts = defaultdict(int)
     for row in strings:
-        left.append(int(row[0]))
-        right.append(int(row[1]))
-    right_counts = Counter(right)
+        left_num, right_num = int(row[0]), int(row[1])
+        left.append(left_num)
+        right_counts[right_num] += 1
     res = 0
     for left_num in left:
         res += (left_num * right_counts[left_num])
@@ -41,8 +40,8 @@ def main(input_filename: str):
     start_part_two = time.time()
     part_two(inp)
     end_time = time.time()
-    print(f"Part one took {start_part_two - start_part_one} seconds")
-    print(f"Part two took {end_time - start_part_two} seconds")
+    print(f"Part one took {start_part_two - start_part_one:.2f} seconds")
+    print(f"Part two took {end_time - start_part_two:.2f} seconds")
 
 if __name__ == "__main__":
     # main("sample.txt")
