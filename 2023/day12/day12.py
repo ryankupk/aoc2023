8dc532dcf41c5241480483f6f1395801aa7d9742
Ryan Kupka <ryankupk@gmail.com>
2024-11-30 19:22:58 -0700

    Not sure, idk when I wrote these changes and idk if they broke anything

--- 2023/day12/day12.py
+++ 2023/day12/day12.py
@@ -19,25 +19,39 @@ def part_one(strings: list[str]) -> None:
         all_perms: list[str] = []
         get_permutation_of_springs(springs, all_perms)
         # reduce to list of groups split by '.'
-        split_perms = list(map(lambda x: x.split('.'), all_perms))
-        for perm in split_perms:
-            # transform perm into pattern
-            perm_pattern = [len(spring_set) for spring_set in perm if spring_set != '']
+        split_perms = list(map(lambda x: list(filter(lambda y: y != '', x.split('.'))), all_perms))
+        filtered = list(filter(lambda x: len(x) == len(patterns), split_perms))
+        for perm in filtered:
             # compare against patterns and count valid
-            if perm_pattern == patterns:
+            if list(map(lambda x: len(x), perm)) == patterns:
                 valid_count += 1
     print(valid_count)
 
  
 def part_two(strings: list[str]) -> None:
-    pass
+    valid_count: int = 0
+    for string in strings: 
+        springs, pattern = string.split(' ')
+        springs = '?'.join([springs] * 5)
+        patterns = list(map(int, pattern.split(','))) * 5
+        all_perms: list[str] = []
+        get_permutation_of_springs(springs, all_perms)
+        # reduce to list of groups split by '.'
+        split_perms = list(map(lambda x: list(filter(lambda y: y != '', x.split('.'))), all_perms))
+        filtered = list(filter(lambda x: len(x) == len(patterns), split_perms))
+        for perm in filtered:
+            # compare against patterns and count valid
+            if list(map(lambda x: len(x), perm)) == patterns:
+                valid_count += 1
+        print("completed a string")
+    print(valid_count)
 
 def main(input_filename: str):
     inp = parse_input(input_filename)
     start_part_one = time.time()
     part_one(inp)
     start_part_two = time.time()
-    part_two(inp)
+    # part_two(inp)
     end_time = time.time()
     print(f"Part one took {start_part_two - start_part_one} seconds")
     print(f"Part two took {end_time - start_part_two} seconds")
