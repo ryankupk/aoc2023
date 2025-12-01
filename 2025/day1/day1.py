f694a74baf74024f67343556366bac60c97f14d2
Ryan Kupka <ryankupk@gmail.com>
2025-11-30 22:32:05 -0700

    welp

--- 2025/day1/day1.py
+++ 2025/day1/day1.py
@@ -25,24 +25,23 @@ def part_two(strings: list[str]) -> None:
     for turn in strings:
         if turn[0] == 'L':
             turn_amount = int(turn[1:])
-            if turn_amount > 100:
-                spins = turn_amount // 100
-                res += spins
-                turn_amount = int(turn[2:])
-            if turn_amount >= start:
-                res += 1
-            start -= turn_amount
-            start %= 100
+            for _ in range(turn_amount):
+                start -= 1
+                if start == 0:
+                    res += 1
+                    start = 100
+                if start == -1:
+                    start = 99
+            if start == 100:
+                start = 0
         elif turn[0] == 'R':
             turn_amount = int(turn[1:])
-            if turn_amount > 100:
-                spins = turn_amount // 100
-                res += spins
-                turn_amount = int(turn[2:])
-            if turn_amount + start >= 100:
-                res += 1
-            start += turn_amount
-            start %= 100
+            for _ in range(turn_amount):
+                start += 1
+                if start == 100:
+                    res += 1
+                    start = 0
+
     print(f"{res=}")
 
 def main(input_filename: str):
@@ -56,5 +55,5 @@ def main(input_filename: str):
     print(f"Part two took {end_time - start_part_two:.2f} seconds")
 
 if __name__ == "__main__":
-    main("sample.txt")
-    # main("input.txt")
+    # main("sample.txt")
+    main("input.txt")
